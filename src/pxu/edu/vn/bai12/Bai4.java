93bf6c63d73b3520e5cc98462ad732f97b244e59
ndhcuongpxu <ndhcuong.hue2023@gmail.com>
2023-07-07 14:50:42 +0700

    thu 6 bai 4 lan 2

--- src/pxu/edu/vn/bai12/Bai4.java
+++ src/pxu/edu/vn/bai12/Bai4.java
@@ -13,6 +13,16 @@ public class Bai4 {
 				System.out.print("n = ");
 				n = sc.nextInt();
 			}while(n <= 0);
+			//Tao mang so nguyen a rong co n phan tu
+			int[] a = new int[n];
+			//Sinh ngau nhien gia tri cac phan tu cho mang a
+			for(int i = 0; i < a.length; i++) {
+				a[i] = (int)(Math.random()*10);
+			}
+			//Xuat gia tri cua mang ra man hinh
+			for(int value: a) {
+				System.out.print(value + " ");
+			}
 		}catch(Exception e) {
 			System.out.println("DA XAY RA LOI TRONG QUA TRINH NHAP DU LIEU");
 		}
